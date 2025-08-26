f7e72d3a8e210c828e0c4657bcaa95ebadbaf990
sugam2060 <sugampudasain3@gmail.com>
2025-08-26 19:17:42 +0530

    ad

--- config/database.ts
+++ config/database.ts
@@ -1,16 +1,36 @@
-export default ({ env }) => ({
-  connection: {
-    client: 'postgres',
-    connection: {
-      host: env('DATABASE_HOST', undefined),   // Strapi Cloud sets this automatically
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', undefined),
-      user: env('DATABASE_USERNAME', undefined),
-      password: env('DATABASE_PASSWORD', undefined),
-      ssl: env.bool('DATABASE_SSL', true) && { rejectUnauthorized: false },
-      schema: env('DATABASE_SCHEMA', 'public'),
+import path from 'path';
+
+export default ({ env }) => {
+  const isProduction = env('NODE_ENV') === 'production';
+
+  const client = isProduction ? 'postgres' : 'sqlite';
+
+  const connections = {
+    sqlite: {
+      connection: {
+        filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      },
+      useNullAsDefault: true,
     },
-    pool: { min: 2, max: 10 },
-    acquireConnectionTimeout: 60000,
-  },
-});
+    postgres: {
+      connection: {
+        host: env('DATABASE_HOST', undefined), // Strapi Cloud sets this automatically
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', undefined),
+        user: env('DATABASE_USERNAME', undefined),
+        password: env('DATABASE_PASSWORD', undefined),
+        ssl: { rejectUnauthorized: false },
+        schema: env('DATABASE_SCHEMA', 'public'),
+      },
+      pool: { min: 2, max: 10 },
+    },
+  };
+
+  return {
+    connection: {
+      client,
+      ...connections[client],
+      acquireConnectionTimeout: 60000,
+    },
+  };
+};
